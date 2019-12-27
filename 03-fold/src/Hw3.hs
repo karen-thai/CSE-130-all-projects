412fadeebe9bf7f64a3f9984d91abf85e3dce40b
o7Ghost <33361213+o7Ghost@users.noreply.github.com>
2019-12-27 14:31:30 -0800

    Add files via upload

---
+++ 03-fold/src/Hw3.hs
@@ -0,0 +1,205 @@
+{- | CSE 130: All about fold.
+
+     For this assignment, you may use the following library functions:
+
+     length
+     append (++)
+     map
+     foldl'
+     foldr
+     unzip
+     zip
+     reverse
+
+  Use www.haskell.org/hoogle to learn more about the above.
+
+  Do not change the skeleton code! The point of this assignment
+  is to figure out how the functions can be written this way
+  (using fold). You may only replace the `error "TBD:..."` terms.
+
+-}
+
+module Hw3 where
+
+import Prelude hiding (replicate, sum)
+import Data.List (foldl')
+
+foldLeft :: (a -> b -> a) -> a -> [b] -> a
+foldLeft = foldl'
+
+--------------------------------------------------------------------------------
+-- | sqSum [x1, ... , xn] should return (x1^2 + ... + xn^2)
+--
+-- >>> sqSum []
+-- 0
+--
+-- >>> sqSum [1,2,3,4]
+-- 30
+--
+-- >>> sqSum [(-1), (-2), (-3), (-4)]
+-- 30
+
+sqSum :: [Int] -> Int
+sqSum xs = foldLeft f base xs
+  where
+   f a x = a + x^2
+   base  = 0
+
+--------------------------------------------------------------------------------
+-- | `pipe [f1,...,fn] x` should return `f1(f2(...(fn x)))`
+--
+-- >>> pipe [] 3
+-- 3
+--
+-- >>> pipe [(\x -> x+x), (\x -> x + 3)] 3
+-- 12
+--
+-- >>> pipe [(\x -> x * 4), (\x -> x + x)] 3
+-- 24
+
+pipe :: [(a -> a)] -> (a -> a)
+pipe fs   = foldLeft f base fs
+  where
+    f a x = a.x
+    base  = \v -> v
+
+--------------------------------------------------------------------------------
+-- | `sepConcat sep [s1,...,sn]` returns `s1 ++ sep ++ s2 ++ ... ++ sep ++ sn`
+--
+-- >>> sepConcat "---" []
+-- ""
+--
+-- >>> sepConcat ", " ["foo", "bar", "baz"]
+-- "foo, bar, baz"
+--
+-- >>> sepConcat "#" ["a","b","c","d","e"]
+-- "a#b#c#d#e"
+
+sepConcat :: String -> [String] -> String
+sepConcat sep []     = ""
+sepConcat sep (x:xs) = foldLeft f base l
+  where
+    f a x            = a ++ sep ++ x
+    base             = x
+    l                = xs
+
+intString :: Int -> String
+intString = show
+
+--------------------------------------------------------------------------------
+-- | `stringOfList pp [x1,...,xn]` uses the element-wise printer `pp` to
+--   convert the element-list into a string:
+--
+-- >>> stringOfList intString [1, 2, 3, 4, 5, 6]
+-- "[1, 2, 3, 4, 5, 6]"
+--
+-- >>> stringOfList (\x -> x) ["foo"]
+-- "[foo]"
+--
+-- >>> stringOfList (stringOfList show) [[1, 2, 3], [4, 5], [6], []]
+-- "[[1, 2, 3], [4, 5], [6], []]"
+
+stringOfList :: (a -> String) -> [a] -> String
+stringOfList f xs = "[" ++ sepConcat ", " (map f xs) ++ "]"
+
+--------------------------------------------------------------------------------
+-- | `clone x n` returns a `[x,x,...,x]` containing `n` copies of `x`
+--
+-- >>> clone 3 5
+-- [3,3,3,3,3]
+--
+-- >>> clone "foo" 2
+-- ["foo", "foo"]
+
+clone :: a -> Int -> [a]
+clone x n = if n == 0 then [] else x : clone x (n-1)
+
+type BigInt = [Int]
+
+--------------------------------------------------------------------------------
+-- | `padZero l1 l2` returns a pair (l1', l2') which are just the input lists,
+--   padded with extra `0` on the left such that the lengths of `l1'` and `l2'`
+--   are equal.
+--
+-- >>> padZero [9,9] [1,0,0,2]
+-- [0,0,9,9] [1,0,0,2]
+--
+-- >>> padZero [1,0,0,2] [9,9]
+-- [1,0,0,2] [0,0,9,9]
+
+padZero :: BigInt -> BigInt -> (BigInt, BigInt)
+padZero l1 l2 = if (length l1) < (length l2) then ((clone 0 ((length l2) - (length l1))) ++ l1, l2) else (l1, clone 0 ((length l1) - (length l2)) ++ l2)
+
+--------------------------------------------------------------------------------
+-- | `removeZero ds` strips out all leading `0` from the left-side of `ds`.
+--
+-- >>> removeZero [0,0,0,1,0,0,2]
+-- [1,0,0,2]
+--
+-- >>> removeZero [9,9]
+-- [9,9]
+--
+-- >>> removeZero [0,0,0,0]
+-- []
+
+removeZero :: BigInt -> BigInt
+removeZero ds 
+        | ds == [] = []
+        | otherwise = if x /= 0 then (x:xs) else removeZero xs 
+          where (x:xs) = ds
+
+--------------------------------------------------------------------------------
+-- | `bigAdd n1 n2` returns the `BigInt` representing the sum of `n1` and `n2`.
+--
+-- >>> bigAdd [9, 9] [1, 0, 0, 2]
+-- [1, 1, 0, 1]
+--
+-- >>> bigAdd [9, 9, 9, 9] [9, 9, 9]
+-- [1, 0, 9, 9, 8]
+
+bigAdd :: BigInt -> BigInt -> BigInt
+bigAdd l1 l2     = removeZero res
+  where
+    (l1', l2')   = padZero l1 l2
+    res          = foldLeft f base args
+    f a b = case (a,b) of
+            ([], (l,r)) -> ((l+r) `div` 10) : ((l +r) `mod` 10) : []
+            ((al:ar),(l,r)) -> ((al+l+r) `div` 10) : ((al+l+r) `mod` 10) : ar 
+    base         = [0]
+    args         = zip (reverse ([0]++l1')) (reverse ([0]++l2'))
+        --where 
+        --pairList :: [a] -> [b] -> [(a,b)]
+        --pairList [] [] = []
+        --pairList (a:as) (b:bs)  = (a,b) : pairList as bs 
+
+--------------------------------------------------------------------------------
+-- | `mulByDigit i n` returns the result of multiplying
+--   the digit `i` (between 0..9) with `BigInt` `n`.
+--
+-- >>> mulByDigit 9 [9,9,9,9]
+-- [8,9,9,9,1]
+
+mulByDigit :: Int -> BigInt -> BigInt
+mulByDigit i n = foldLeft bigAdd [0] (clone n i)
+
+--------------------------------------------------------------------------------
+-- | `bigMul n1 n2` returns the `BigInt` representing the product of `n1` and `n2`.
+--
+-- >>> bigMul [9,9,9,9] [9,9,9,9]
+-- [9,9,9,8,0,0,0,1]
+--
+-- >>> bigMul [9,9,9,9,9] [9,9,9,9,9]
+-- [9,9,9,9,8,0,0,0,0,1]
+
+bigMul :: BigInt -> BigInt -> BigInt
+bigMul l1 l2 = res
+  where
+    (_, res) = foldLeft f base args
+    --f :: (Int,bigInt) -> bigInt -> (Int,bigInt)
+    f (al,br) x    = (al+1 , (bigAdd br (x ++ clone 0 al)))
+    base     = (0, [])
+    args     = list l1 (reverse l2)
+        where
+        list :: BigInt -> BigInt -> [BigInt]
+        list _ [] = []
+        list a (b:bs) = mulByDigit b a : list a bs
